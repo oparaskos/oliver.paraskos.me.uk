e17698fc00b9588093df828eb84fb06f28441bb8
Oliver Paraskos <oparaskos@googlemail.com>
2020-10-18 20:35:58 +0100

    .,.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # React JS Resume Web App Template      
-![ReactJS Resume Website Template](resume-screenshot.jpg?raw=true "ReactJS Resume Website Template")
+![ReactJS Resume Website Template](resume-screenshot.png?raw=true "ReactJS Resume Website Template")
 ### <a href="https://react-resume-template.herokuapp.com/">LIVE DEMO</a> 
 
 ## Description
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,56 +1,88 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Socials from './Socials';
+import { Link as LinkOriginal } from "react-scroll";
 
-class Header extends Component {
-  render() {
+const Link = ({ to, children }) => <LinkOriginal
+   to={to}
+   smooth={true}
+   duration={500}
+   style={{cursor : "pointer"}}
+>
+   {children}
+</LinkOriginal>
 
-   if (this.props.data && this.props.data.basics){
-      var name = this.props.data.basics.name;
-      var occupation= this.props.data.basics.label;
-      var description= this.props.data.basics.summary;
-      var city= this.props.data.basics.location.city;
-      var profiles = this.props.data.basics.profiles;
+
+function navClass(y, h) {
+   if ( (y > h*.20) && (y < h) && (window.outerWidth > 768 ) ) {
+      return 'transparent';
+   }
+   else {
+      if (y < h*.20) {
+         return '';
+      }
+      else {
+         return 'opaque';
+      }
+   }
+}
+
+var listener;
+const Header = ({ data }) => {
+   if (data && data.basics) {
+      var name = data.basics.name;
+      var occupation = data.basics.label;
+      var description = data.basics.summary;
+      var city = data.basics.location.city;
+      var profiles = data.basics.profiles;
    } else {
-      return <div>Something Went Wrong!</div>
+      return <div>Loading&ellip;</div>
    }
 
-    return (
+   const [navClassName, setNavClassName] = useState();
+   if (listener) window.removeEventListener(listener);
+   listener = window.addEventListener('scroll', () => {
+      const y = window.scrollY;
+      const h = document.documentElement.clientHeight;
+      const newClassName = navClass(y, h);
+      if (newClassName !== navClassName) setNavClassName(newClassName);
+   }, [navClassName]);
+
+   return (
       <header id="home">
 
-      <nav id="nav-wrap">
+         <nav id="nav-wrap" className={navClassName}>
 
-         <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
+            <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+            <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
 
-         <ul id="nav" className="nav">
-            <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
-            <li><a className="smoothscroll" href="#about">About</a></li>
-	         <li><a className="smoothscroll" href="#resume">Resume</a></li>
-            {this.props.data.portfolio && <li><a className="smoothscroll" href="#portfolio">Works</a></li>}
-            {this.props.data.testimonials && <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>}
-            <li><a className="smoothscroll" href="#contact">Contact</a></li>
-         </ul>
-
-      </nav>
-
-      <div className="row banner">
-         <div className="banner-text">
-            <h1 className="responsive-headline">I'm {name}.</h1>
-            <h3>I'm a {city} based <span>{occupation}</span> {description}.</h3>
-            <hr />
-            <ul className="social">
-               <Socials profiles={profiles} />
+            <ul id="nav" className="nav">
+               <li><Link to="home">Home</Link></li>
+               <li><Link to="about">About</Link></li>
+               <li><Link to="resume">Resume</Link></li>
+               {data.portfolio && <li><Link to="portfolio">Works</Link></li>}
+               {data.testimonials && <li><Link to="testimonials">Testimonials</Link></li>}
+               <li><Link to="contact">Contact</Link></li>
             </ul>
+
+         </nav>
+
+         <div className="row banner">
+            <div className="banner-text">
+               <h1 className="responsive-headline">I'm {name}.</h1>
+               <h3>I'm a {city} based <span>{occupation}</span> {description}.</h3>
+               <hr />
+               <ul className="social">
+                  <Socials profiles={profiles} />
+               </ul>
+            </div>
          </div>
-      </div>
 
-      <p className="scrolldown">
-         <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
-      </p>
+         <p className="scrolldown">
+            <Link to="about"><i className="icon-down-circle"></i></Link>
+         </p>
 
-   </header>
-    );
-  }
+      </header>
+   );
 }
 
 export default Header;
